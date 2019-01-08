fa79c4bd12f3fde6b00eb3ff750e25924ca51569
Naif <naif@naif.io>
2019-01-08 19:11:41 +0300

    publish config

---
+++ config/address_autocomplete.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key' => env('ADDRESS_AUTOCOMPLETE_API_KEY'),
+    'language' => env('ADDRESS_AUTOCOMPLETE_LANGUAGE'),
+];
--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Naif\AddressAutocomplete;
 
+use Illuminate\Support\Facades\Config;
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
@@ -15,7 +16,14 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/address_autocomplete.php' => base_path('config/address_autocomplete.php'),
+            ], 'config');
+        }
+
         Nova::serving(function (ServingNova $event) {
+            Nova::script('address_autocomplete_gmaps', $this->googleMapsSource());
             Nova::script('address_autocomplete', __DIR__.'/../dist/js/field.js');
             Nova::style('address_autocomplete', __DIR__.'/../dist/css/field.css');
         });
@@ -28,6 +36,17 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__.'/../config/address_autocomplete.php', 'address_autocomplete');
+    }
+
+    private function googleMapsSource()
+    {
+        return vsprintf(
+            'https://maps.googleapis.com/maps/api/js?key=%s&language=%s&libraries=places',
+            [
+                Config::get('address_autocomplete.api_key'),
+                Config::get('address_autocomplete.language'),
+            ]
+        );
     }
 }
